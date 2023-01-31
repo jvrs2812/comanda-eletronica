3ea4f265180fa6dec04c16c964277e8096d4556a
Joao Sousa <jvrs_souza@hotmail.com>
2023-01-30 22:04:42 -0300

    feat: adicionando novos repositorios ao entity scan

--- src/main/java/com/comanda/comanda/ComandaApplication.java
+++ src/main/java/com/comanda/comanda/ComandaApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"com.comanda.comanda.Product.Repository", "com.comanda.comanda.Category.repository", "com.comanda.comanda.Table.Repository"})
+@EntityScan(basePackages = {"com.comanda.comanda.Product.Repository",
+		                    "com.comanda.comanda.Category.repository",
+		                    "com.comanda.comanda.Table.Repository",
+		                    "com.comanda.comanda.Commands.Repository"})
 public class ComandaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ComandaApplication.class, args);
